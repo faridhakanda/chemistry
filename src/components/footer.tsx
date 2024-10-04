a78a2bbfda97e9cb3d657f6021aa162c08594d60
faridhakanda <faridakanda1729@gmail.com>
2024-10-04 17:41:57 +0600

    first with link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 const Footer = () => {
   return (
     <div className="bg-slate-950 text-slate-100 p-2 text-center">
         <div>
             <h1>Powered by Farid Akanda.</h1>
             <h1>&copy; All Right Reserved 2024.</h1>
+            <Link href="/">Go to Home</Link>
         </div>
     </div>
   )
