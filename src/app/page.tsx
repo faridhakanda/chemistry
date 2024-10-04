ee9300ede08ff8a97ce2bb5fcf191f5d4736fd88
faridhakanda <faridakanda1729@gmail.com>
2024-10-04 18:03:24 +0600

    image add for profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="bg-slate-50 shadow-lg min-h-screen p-12">
       <div className="justify-center text-center">
         <div className="p-3">
-          <Image className="rounded-full mx-auto" src="/user_logo.png" alt="ismail" width="100" height="100" />
+          <Image className="rounded mx-auto" src="/ismail.jpg" alt="ismail" width="150" height="150" />
           <div>
             <h1 className="font-semibold text-2xl">Ismail Hossain Bijoy</h1>
             <h1 className="text-justify mx-auto sm:w-5/4 md:w-5/4 lg:w-2/4 xl:w-2/4 2xl:w-2/4 3xl:w-2/4">Hi there, I am Bijoy. I am a chemist. I have more than 2 years of experience in analytical chemistry. Currently I am working in a college as a assistant researcher in field of organic chemistry. My interest in organic chemistry and computational chemistry.</h1>
